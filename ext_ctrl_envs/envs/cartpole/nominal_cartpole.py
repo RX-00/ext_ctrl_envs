5b5a6e65b2fa52428ebcb7760023993371f2a52c
Roy X <royx@bu.edu>
2023-08-12 18:23:22 -0400

    time punish reward function, best results so far

--- ext_ctrl_envs/envs/cartpole/nominal_cartpole.py
+++ ext_ctrl_envs/envs/cartpole/nominal_cartpole.py
@@ -49,9 +49,9 @@ class NominalCartpoleEnv(MujocoEnv, utils.EzPickle):
         ob = self._get_obs()
         
         # termination state conditions
-        terminated = bool(not np.isfinite(ob).all() or 
-                          (np.abs(ob[1]) > 0.01) or  # keep pend upright
-                          (np.abs(ob[0]) > 0.01))    # keep cart at near origin
+        terminated = bool(not np.isfinite(ob).all()) #or 
+                          #(np.abs(ob[1]) > 0.01) or  # keep pend upright
+                          #(np.abs(ob[0]) > 0.01))    # keep cart at near origin
         
         if self.render_mode == "human":
             self.render()
@@ -107,11 +107,11 @@ class NominalCartpoleEnv(MujocoEnv, utils.EzPickle):
                                             obs_val=obs[indx], 
                                             weight_h=weight_h,
                                             alpha=interm_weight)
-
-        #reward += self.calc_reward(ref_val=0.0,
-        #                           obs_val=np.abs(obs[0])+np.abs(obs[1]),
-        #                           weight_h=2,
-        #                           alpha=1000) # increase to make it more precise to earn reward
+        
+        # punish for time cost
+        if (interm_weights[4] > 250 and
+            (np.abs(obs[0]) > 0.01 or np.abs(obs[1] > 0.01))):
+            reward -= interm_weights[4] / 50
         
         # termination conditions
         if (not np.isfinite(obs).all() or #r_trunc > epsilon):
@@ -130,6 +130,64 @@ class NominalCartpoleEnv(MujocoEnv, utils.EzPickle):
 
         return ob, reward, terminated, False, {}
 
+    # Step forward in simulation, given an action and trajectory and time
+    def step_traj_track_t(self, a, x, x_dot, theta, theta_dot, u, weight_h, interm_weights, t):
+        self.do_simulation(a, self.frame_skip)
+        ob = self._get_obs()
+        reward = 0
+        terminated = False
+        '''
+        Calculate reward based on max(squared exponential of tracking error)
+        on trajectory state tracking
+
+        # NOTE: ob is organized as follows,
+                [x, theta, x_dot, theta_dot], we add action a to get
+                obs
+                [x, theta, x_dot, theta_dot, a]
+                 0    1      2      3        4
+        '''
+        obs = np.append(ob, a)
+        ref_obs = [x, theta, x_dot, theta_dot, u]
+
+        for indx, interm_weight in enumerate(interm_weights):
+            if (indx == 0 or indx == 1):
+                # punish for not being cart 0 and pend 0
+                # punish 10 comes from 5 terms in state vector times 2
+                #reward -= 10 * np.abs(obs[indx] - 0.0)**2
+                punish = 4 # 10
+                reward -= -punish * np.exp(-10*np.abs(obs[indx])**2) + punish
+                # reward for being close to trajectory
+                reward += self.calc_reward(ref_val=0.0,
+                                           obs_val=obs[indx],
+                                           weight_h=3,
+                                           alpha=100) # increase to make it more precise to earn reward
+                        
+            if (indx < 4): # 4
+                reward += self.calc_reward(ref_val=ref_obs[indx],
+                                            obs_val=obs[indx], 
+                                            weight_h=weight_h,
+                                            alpha=interm_weight)
+
+        # punish for time cost
+        reward -= 0.1 * np.exp(0.05*t - 10.0) # punish more after timestep 250
+        
+        # termination conditions
+        if (not np.isfinite(obs).all() or #r_trunc > epsilon):
+            np.abs(ref_obs[0] - obs[0]) > 0.25 or # 0.25 is too tight
+            np.abs(ref_obs[1] - obs[1]) > 0.25 or # 0.25 is too tight
+            #np.abs(ref_obs[2] - obs[2]) > 0.25 or # 0.25 is too tight
+            #np.abs(ref_obs[3] - obs[3]) > 0.25 or # 0.25 is too tight
+            np.abs(obs[1]) > np.pi):
+            terminated = True
+
+        if terminated:
+            reward = 0
+        # to render or not to render
+        if self.render_mode == "human":
+            self.render()
+
+        return ob, reward, terminated, False, {}
+    
 
     # Initial state of the system
     def reset_model(self):
